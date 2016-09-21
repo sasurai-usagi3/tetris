c26903f88c0b039119c807ddd2279ef62145d95a
宇山拓夢 <sasurai.usagi3@gmail.com>
2016-09-21 21:23:42 +0900

    盤の作成

---
+++ js/application.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  var table = document.createElement("table");
+  var thead = document.createElement("thead");
+  var tbody = document.createElement("tbody");
+  var cell = [];
+
+  for(var i = 0; i < 21; ++i) {
+    var tr = document.createElement("tr");
+    cell[i] = [];
+    for(var j = 0; j < 10; ++j) { 
+      var td = document.createElement("td");
+      tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
+  }
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  document.body.appendChild(table);
+}
